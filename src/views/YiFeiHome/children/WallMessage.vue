59a94d7a70da3f4d23ff08d9b7f9ea4c79b0f88e
ZRMYDYCG <547471919@qq.com>
2024-06-06 11:43:34 +0800

    feat:声明数据模块

--- src/views/YiFeiHome/children/WallMessage.vue
+++ src/views/YiFeiHome/children/WallMessage.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { wallType,label } from '@/utils/data'
 
+console.log(wallType, label)
 </script>
 
 <template>
-  <div class="wall-message"></div>
+  <div class="wall-message">
+    <p class="title"></p>
+  </div>
 </template>
 
 <style scoped>
 .wall-message {
   min-height: 800px;
+  padding-top: 52px;
 }
 </style>
